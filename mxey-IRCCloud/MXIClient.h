ea84ff93d193d4537de839b89b1e997646760a49
Maximilian Gaß <mxey@mxey.net>
2014-03-26 17:41:33 +0000

    Small fixes

--- mxey-IRCCloud/MXIClient.h
+++ mxey-IRCCloud/MXIClient.h
@@ -15,10 +15,8 @@
 @interface MXIClient : NSObject <MXIClientTransportDelegate>
 - (void)loginWithEmail:(NSString *)email andPassword:(NSString *)password;
 
-- (void)sendMessage:(NSString *)message toBufferName:(NSString *)bufferName onConnectionId:(NSNumber *)connectionId;
-
-@property (nonatomic) NSMutableDictionary *servers;
+@property(nonatomic) NSMutableDictionary *servers;
 
 @property(nonatomic) NSMutableArray *serverOrder;
-@property (nonatomic) id<MXIClientDelegate> delegate;
+@property(nonatomic) id <MXIClientDelegate> delegate;
 @end
--- mxey-IRCCloud/MXIClient.m
+++ mxey-IRCCloud/MXIClient.m
@@ -7,6 +7,7 @@
 //
 
 #import "MXIClient.h"
+#import "MXIClientServer.h"
 
 @interface MXIClient ()
 @property(nonatomic) MXIClientTransport *transport;
