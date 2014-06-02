9d159a0c38bd0cd836f487a6c15c4ef80a6d8039
Maximilian Gaß <mxey@mxey.net>
2014-06-02 13:54:40 +0200

    Add missing login sheet controller

---
+++ mxey-IRCCloud/MXILoginSheetController.h
@@ -0,0 +1,19 @@
+//
+//  MXILoginSheetController.h
+//  mxey-IRCCloud
+//
+//  Created by Maximilian Gaß on 31.05.14.
+//  Copyright (c) 2014 Maximilian Gaß. All rights reserved.
+//
+
+
+
+@class MXIAppDelegate;
+
+@interface MXILoginSheetController : NSWindowController
+@property(weak) IBOutlet NSTextField *accountField;
+@property(weak) IBOutlet NSSecureTextField *passwordField;
+
+@property(nonatomic, copy) NSString *password;
+@property(nonatomic, copy) NSString *account;
+@end
