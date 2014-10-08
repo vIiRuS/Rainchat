4ebb50c0f52d67914d75216fddd57f9165d1b4bd
Phillip Thelen <viirus@pherth.net>
2014-10-08 13:36:51 +0200

    scroll to end of webview on new messages

--- Rainchat/MXIBufferWebView.m
+++ Rainchat/MXIBufferWebView.m
@@ -16,20 +16,29 @@
     [self setEditable:YES];
 }
 
+- (void) scrollToElement:(DOMHTMLElement*)element {
+    NSRect myRect = element.boundingBox;
+    [self stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:@"window.scrollTo(%d,%d);", (int)myRect.origin.x, (int)myRect.origin.y]];
+}
+
 - (void)appendItemWithContent:(NSString *)content {
     DOMHTMLElement *itemsNode = (DOMHTMLElement *) [[[self mainFrame] DOMDocument] getElementById:@"items"];
     DOMHTMLElement *newNode = (DOMHTMLElement *) [[[self mainFrame] DOMDocument] createElement:@"li"];
     [newNode setInnerHTML:content];
     [itemsNode appendChild:newNode];
+    [self scrollToElement:newNode];
 }
 
 -(void)setItems:(NSArray *)items {
     DOMHTMLElement *itemsNode = (DOMHTMLElement *) [[[self mainFrame] DOMDocument] getElementById:@"items"];
+    [itemsNode setInnerHTML:@""];
+    DOMHTMLElement *newNode;
     for (NSString *item in items) {
-        DOMHTMLElement *newNode = (DOMHTMLElement *) [[[self mainFrame] DOMDocument] createElement:@"li"];
+        newNode = (DOMHTMLElement *) [[[self mainFrame] DOMDocument] createElement:@"li"];
         [newNode setInnerHTML:item];
         [itemsNode appendChild:newNode];
     }
+    [self scrollToElement:newNode];
 }
 
 @end
